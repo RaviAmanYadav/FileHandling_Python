ce178cdf9b4b7596ec65fdd97249fe2cd51647a8
RaviAmanYadav <amanbittuyadav@gmail.com>
2026-03-26 00:29:01 +0530

    add Student and show student completed with Try and expect

---
+++ student.py
@@ -0,0 +1,73 @@
+import json
+import os
+
+FILE = "students.json"
+
+
+def addStudent():
+    # Load existing data
+    if os.path.exists(FILE):
+        try:
+            with open(FILE, "r") as f:
+                data = json.load(f)
+        except:
+            data = {}  # if the file is empty
+    else:
+        data = {}
+
+    # Auto generated roll number
+    if data:
+        last_roll = max(map(int, data.keys()))
+        new_roll = last_roll + 1
+    else:
+        new_roll = 1
+
+    # Input
+    studentName = input("Enter student name => ")
+    fatherName = input("Enter Father's name => ")
+    motherName = input("Enter mother's name => ")
+
+    # Store data
+    data[str(new_roll)] = {
+        "name": studentName,
+        "father": fatherName,
+        "mother": motherName,
+    }
+
+    # Save
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
+
+    print(f"Student added with roll number : {new_roll} Successfully.\n")
+
+
+def showStudent():
+    if not os.path.exists(FILE):
+        print("Data not found")
+        return
+    else:
+        with open(FILE, "r") as f:
+            data = json.load(f)
+
+        for roll, details in data.items():
+            print(f"Roll number => {roll}")
+            print(f"Name => {details["name"]}")
+            print(f"Father's Name => {details["father"]}")
+            print(f"Mother's Name => {details["mother"]}")
+            print("-" * 20)
+
+
+while True:
+    print("1. Add student")
+    print("2. Show student")
+    print("3. Exit.")
+    choice = int(input("Enter your choice => "))
+    match choice:
+        case 1:
+            addStudent()
+        case 2:
+            showStudent()
+        case 3:
+            exit()
+        case _:
+            print("Invalid operations")
