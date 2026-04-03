2e9c3ae36978478104651518abc31f0f0c317448
RaviAmanYadav <amanbittuyadav@gmail.com>
2026-04-04 03:59:57 +0530

    Add of student completed

---
+++ student2/student.py
@@ -0,0 +1,68 @@
+import os
+import json
+from typing import Dict
+
+FILE = os.path.join("FileHandling_Python", "student2", "student.json")
+
+
+class Parents:
+    def __init__(self, fname: str, mname: str):
+        self.fname = fname
+        self.mname = mname
+
+    def to_dict(self) -> Dict:
+        return {"father_name": self.fname, "mother_name": self.mname}
+
+
+class StudentManage:
+    def __init__(self, filePath: str):
+        self.filePath = filePath
+        self.data = self._load_data()
+
+    def _load_data(self) -> Dict:
+        if os.path.exists(self.filePath):
+            try:
+                with open(self.filePath, "r") as f:
+                    return json.load(f)
+            except json.JSONDecodeError:
+                return {}
+        return {}
+
+    def _generate_roll(self) -> int:
+        if not self.data:
+            return 1
+        return max(map(int, self.data.keys())) + 1
+
+    def _save_data(self):
+        """Saving data into student.json"""
+        with open(self.filePath, "w") as f:
+            json.dump(self.data, f, indent=4)
+
+    def addStudent(self):
+        name = input("Enter student name => ")
+        fname = input("Enter father's name => ")
+        mname = input("Enter mother's name => ")
+
+        parent = Parents(fname, mname)
+        roll = self._generate_roll()
+
+        self.data[str(roll)] = {"name": name, "parent": parent.to_dict()}
+
+        self._save_data()
+        print(f"student roll number {roll} has been successfully admitted.")
+
+
+if __name__ == "__main__":
+    manager = StudentManage(FILE)
+
+    while True:
+        print("1. Add student")
+        print("2.Exit")
+        choice = int(input("Enter your choice => "))
+        match choice:
+            case 1:
+                manager.addStudent()
+            case 2:
+                exit()
+            case _:
+                print("Invalid operation")
